528f194f6248710096c386a0f1da7609c3c921ec
ilionroot <ilion.root@gmail.com>
2022-08-06 00:31:41 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import puppeteer from "puppeteer";
+
+import express from "express";
+
+const app = express();
+
+async function initialize(req, res) {
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    "https://cademinhaentrega.com.br/gfl/tracking/D/37114335881",
+    {
+      waitUntil: "load",
+    }
+  );
+
+  await page.waitForTimeout(1000);
+  await page.click(".md.accordion-group-expand-compact.hydrated");
+
+  const items = await page.evaluate(() => {
+    const items = Array.from(
+      document.querySelectorAll(
+        ".ion-no-padding.item.md.item-fill-none.in-list.hydrated.item-label"
+      )
+    );
+    return items
+      .map((item) => {
+        return {
+          info: item.querySelector("h4")?.innerText,
+          date: item.querySelector("h5")?.innerText,
+          hour: item.querySelector("h6")?.innerText,
+        };
+      })
+      .reverse();
+  });
+  res.send(items);
+  await browser.close();
+}
+
+app.get("/", initialize);
+
+app.listen(4000, () => {
+  console.log("server running on port 4000");
+});
